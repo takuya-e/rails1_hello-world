0966123383fbec8fb4aef807c4434918a37f9961
takuya_endu <takuya.e.0702@gmail.com>
2020-11-16 16:39:57 +0900

    rails1 first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,11 @@ class User
   end
 
   def introduce
+    
     <<~EOS
-
     私の名前は#{@first_name + @last_name}です。
     誕生日は#{@birthday}で、年齢は#{@age}歳。
     出身地は#{@birthplace}で、趣味は#{@hobby}です。
-
     EOS
 
   end
